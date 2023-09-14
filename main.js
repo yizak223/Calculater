8c905fb45af8eebbf015e8ba7981e95be1fe1f6a
yizak223 <139069183+yizak223@users.noreply.github.com>
2023-09-15 01:50:13 +0300

    comit

--- main.js
+++ main.js
@@ -16,6 +16,10 @@ document.body.innerHTML+=
                 <button class='press_num'>2</button>
                 <button class='press_num'>3</button> 
                 <button class='press_num'>0</button> 
+                <div>
+                    <button id='dlt'>delete</button>
+                    <button id='rst'>reset</button>
+                </div>
             </div>
             <div id="btn_math">
                 <button class='press_operator'>X</button>
@@ -42,46 +46,79 @@ const result=document.querySelector('#result')
 const PRESS_NUM=document.querySelectorAll('.press_num')
 const PRESS_OPERATOR=document.querySelectorAll('.press_operator')
 
-let isThereOperator=false
-
 for (const num of PRESS_NUM) {
-
-        result.innerHTML=
-        `<span id='numArea'></span>
-        <span id='operatorArea'></span>`
-    num.addEventListener('click',()=>
+    result.innerHTML=
+        `<span id='numArea'></span>`
+        num.addEventListener('click',()=>
     {
         numArea.innerHTML+=
         `${num.textContent}`  
     }) 
 }
 
-let c=0
 for (const operator of PRESS_OPERATOR) {
-
     operator.addEventListener('click',()=>
     {
-        if(isThereOperator==false)
-        {
-           operatorArea.innerHTML=
+            numArea.innerHTML+=
             `${operator.textContent}` 
-            isThereOperator=true  
-            if (isThereOperator==true) {
-                result.innerHTML+=
-            `<span id=secondNumArea${c++}></span>`
-            }
-        }},{once:true})
+        })
+}
+
+rst.addEventListener('click',()=>{
+    result.innerHTML=
+        `<span id='numArea'></span>`
+})
+console.log(numArea.textContent);
+dlt.addEventListener('click',()=>{
+    if(!isNaN(numArea.textContent/10))
+    {
+      numArea.textContent=Math.floor(numArea.textContent/10)  
+    }
+    else{   
+        let stringNum=`${numArea.textContent}`
+        numArea.textContent=stringNum.substring(0,stringNum.length-1)
+    }
+})
+
+// let isThereOperator=false
+
+// for (const num of PRESS_NUM) {
+
+//         result.innerHTML=
+//         `<span id='numArea'></span>`
+//     num.addEventListener('click',()=>
+//     {
+//         numArea.innerHTML+=
+//         `${num.textContent}`  
+//     }) 
+// }
+
+// let c=0
+// for (const operator of PRESS_OPERATOR) {
+
+//     operator.addEventListener('click',()=>
+//     {
+//         // if(isThereOperator==false)
+//         // {
+//             numArea.innerHTML=
+//             `${operator.textContent}` 
+//             isThereOperator=true  
+//             // if (isThereOperator==true) {
+//             //     result.innerHTML+=
+//             // `<span id=secondNumArea${c++}></span>`
+//             // }
+//         },{once:true})
        
 
-            for (const num of PRESS_NUM) {
-                // removeEventListener('click',operator)
-                num.addEventListener('click',()=>
-                {
-                    secondNumArea0.innerHTML+=
-                    `${num.textContent}`  
-                },{once:true}) 
-            }
-        }
+//             for (const num of PRESS_NUM) {
+//                 // removeEventListener('click',operator)
+//                 num.addEventListener('click',()=>
+//                 {
+//                     secondNumArea0.innerHTML+=
+//                     `${num.textContent}`  
+//                 },{once:true}) 
+//             }
+//         }
     
 
 
