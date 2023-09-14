fda423802bddf40c0653c801d8e4c1336f5d334e
yizak223 <139069183+yizak223@users.noreply.github.com>
2023-09-14 21:55:28 +0300

    comit

--- main.js
+++ main.js
@@ -42,28 +42,95 @@ const result=document.querySelector('#result')
 const PRESS_NUM=document.querySelectorAll('.press_num')
 const PRESS_OPERATOR=document.querySelectorAll('.press_operator')
 
+let isThereOperator=false
+
 for (const num of PRESS_NUM) {
+
         result.innerHTML=
         `<span id='numArea'></span>
         <span id='operatorArea'></span>`
     num.addEventListener('click',()=>
     {
-         numArea.innerHTML+=
-        `${num.textContent}` 
+        numArea.innerHTML+=
+        `${num.textContent}`  
     }) 
 }
 
-let counter=0
+let c=0
 for (const operator of PRESS_OPERATOR) {
 
     operator.addEventListener('click',()=>
     {
-        operatorArea.innerHTML=
-        `${operator.textContent}` 
- 
-    })
- 
-}
+        if(isThereOperator==false)
+        {
+           operatorArea.innerHTML=
+            `${operator.textContent}` 
+            isThereOperator=true  
+            if (isThereOperator==true) {
+                result.innerHTML+=
+            `<span id=secondNumArea${c++}></span>`
+            }
+        }},{once:true})
+       
+
+            for (const num of PRESS_NUM) {
+                // removeEventListener('click',operator)
+                num.addEventListener('click',()=>
+                {
+                    secondNumArea0.innerHTML+=
+                    `${num.textContent}`  
+                },{once:true}) 
+            }
+        }
+    
+
+
   
+
+
+
+
+
+
+
+// if (isThereOperator==false) {
+//     for (const num of PRESS_NUM) {
+//                 result.innerHTML=
+//                 `<span id='numArea'></span>
+//                 <span id='operatorArea'></span>`
+//             num.addEventListener('click',()=>
+//                 {
+//                     console.log(isThereOperator);
+//                     numArea.innerHTML+=
+//                     `${num.textContent}` 
+//                 }) 
+//             }
+//         }
+// else if(isThereOperator){
+//     console.log(isThereOperator);
+//     for (const num of PRESS_NUM) {
+//     result.innerHTML+=
+//     `<span id='secondNumArea'></span>`
+//     num.addEventListener('click',()=>
+//     {
+//         secondNumArea.innerHTML+=
+//         `${num.textContent}` 
+//     })
+//     }
+// }     
+    
+
+
+    
+
+// result.innerHTML=
+// `<span id='numArea'></span>
+// <span id='operatorArea'></span>`
+// num.addEventListener('click',()=>
+// {
+//  console.log(isThereOperator);
+//     numArea.innerHTML+=
+//     `${num.textContent}` 
+// })
 
     
